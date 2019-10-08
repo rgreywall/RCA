8c22178e81f0cbc083f4b667824f5b5747577c90
Anna <dorogaya_anna@ukr.net>
2019-10-08 10:10:58 -0700

    Anna's change

---
+++ Styles/AppStyles.js
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+var styles = StyleSheet.create({
+  app:{
+    flex:1,
+    justifyContent:"center",
+    alignItems:"center"
+  }
+})
+
+export default styles;
